b3e0dd12bc311cd9839162c26267abbeceff2441
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-10-21 20:05:13 +0200

    change gamewrap height and width, erase  comments

--- js/script.js
+++ js/script.js
@@ -60,7 +60,7 @@ function clickBox(){
     if (score === (allCell.length - bombsNumb)) {
       end = true;
       result = `
-      Hai vinto con ${score} punti su ${allCell.length}
+      Hai vinto totalizzando ${score} punti su ${allCell.length - bombsNumb}
       `;
     }
   } else {
@@ -68,10 +68,9 @@ function clickBox(){
     score = score - 1;
     end = true;
     result = `
-    Hai perso con ${score} punti su ${allCell.length}
+    Hai perso totalizzando ${score} punti su ${allCell.length - bombsNumb}
     `;
   }
-
   if (end === true) {
     endGame(result);
  }
@@ -103,71 +102,4 @@ function reset() {
   score = 0;
   document.querySelector(".output").innerText = "";
   gameWrap.innerText = "";
-  console.log(gameWrap);
 }
-
-/*
-
-VARIABILI GLOBALI
-1 dichiarazione array bombe vuoto
-2 dichiarazione numero bombe
-3 dichiarazione punteggio che inizialmente è zero
-
-WORKFLOW
-1)Pagina iniziale: h2 centrale con scritta "scegli il livello di difficoltà e premi Start"
- 1.Al clicK di start, cancellare il contenuto di h2
- 2. Azzerare il punteggio
- 3. Pulire il campo con il punteggio finale
- 2.Avviare la funzione INIT
-
- FUNZIONE INIT
- 1. Genera un array con le bombe: l'array è lungo quanto il numero delle bombe scelto che dichiariamo come una costante globale. L'array lo dichiariamo fuori inizialmente vuoto.
- Chiamiamo la funzione per generare le bombe.
- 1. Generare le celle
-
-FUNZIONE GENERA BOMBE
-1. estraggo un numero da 1 al numero totale delle celle fino a che il mio array è lungo quanto il numero delle bombe dichiarato globalmente.
-  Se il numero non è contenuto nell'array bombe, lo pusho dentro altrimenti ne estraggo un altro.
-  la funzione ritorna l'array bombe
-
-FUNZIONE CLICKBOX
-1 aumento il numero dello score (variabile globale)
-3 Se ho cliccato sulla cella con un id NON incluso nell'array bombe:
-  1 aggiungo alla cella  la classe .change che la colora.
-  2 Controllo il valore dello score;
-    se lo score è uguale (numero totali celle - numero bombe) allora:
-    1. isWinner = true; 
-    2. chiamo funzione FINE GIOCO
-
-Altrimenti (se ho cliccato su una cella con l'id incluso nell'array bombe):
-  score = score -1;
-  isWinner = false;
-  1 Chiamo funzione FINE GIOCO
-
- if is Winner = true; 
-  const result = "hai vinto con "score" tentativi su totalBomb"
-if is Winner = false
-  const result = "hai perso con "score" tentativi su totalBomb"
-
-
-FUNZIONE FINE GIOCO ha bisogno dell'attribuito result
-1 Mostra tutte le bombe chiamando la funzione ShowBombs
-1 Congela griglia: chiamo funzione per congelare
-2 Scrivo dentro h2 il result risultato.
-
-FUNZIONE PER CONGELARE
-1 crea un elemento div
-2 all'elemento div dò classe .freeze (nel CSS questa ha position absolute, top 0 left 0, colore grigino trasparente e width ed height 100%)
-3 inserisco il div nel main wrapper a cui nel CSS ho dato position absolute
-
-FUNZIONE SHOWBOMBS
-1. ciclo ogni elemento della griglia da 0 al totalecelle
-  1. se this.idElement è incluso nell'array bombe:
-      aggiungo classe .bomb(classe che ho messo nel css e colora il div di rosso)
-
-FUNZIONE RESET
-1. Azzerare il punteggio
-2. Pulire il campo con il punteggio finale H2
-
-*/
-
